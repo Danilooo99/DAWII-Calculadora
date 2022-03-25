3983618f18cbab443c6f80b50de5c095885d7bf3
Danilooo99 <danilo.informatica.99@gmail.com>
2022-03-25 01:24:57 +0000

    Added basic operations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,12 +6,69 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Ejercicio con Texto';
-  text1="1";
-  text2="2";
-  concat="X";
+  titulo = "DAW II - Calculadora.";
+  autor = "Realizada por Danilo Rivero Pérez";
+  pantalla="0";
+  resultado="";
+  control=false;
+  error=false;
+  porciento=0;
 
-  concatenate():void{
-    this.concat=this.text1+this.text2;
+  operacion(numero:string):void {
+    
+    if(this.pantalla === "0" || this.pantalla === "Syntax Error"){
+      this.pantalla="";
+    } 
+
+    if(this.control){
+      if(this.pantalla!=="0"){
+        this.pantalla="";
+      }
+    }
+
+    this.control=false;
+    this.pantalla += numero;
+
+    for(var i=0;i<this.pantalla.length;i++){
+      if(this.pantalla.charAt(0)==="*" || this.pantalla.charAt(0)==="+" || this.pantalla.charAt(0)==="/" || this.pantalla.charAt(0)==="%") {
+        this.pantalla="0";
+        break;
+      } else {
+        if(this.pantalla[i]==="*" || this.pantalla[i]==="+" || this.pantalla[i]==="-" || this.pantalla[i]==="/" || this.pantalla[i]==="%") {
+          if(this.pantalla[i] === this.pantalla[i+1]) {
+            this.error=true;
+          }
+        }
+      }
+    }
+
   }
+
+  resultadoOperacion():void {
+    if(this.error){
+      this.pantalla="Syntax Error";
+      this.error=false;
+    } else {
+      this.control=true;
+      //alert(this.pantalla.concat(this.porciento.toString()));
+      this.resultado = eval(this.pantalla/*.concat(this.porciento.toString())*/);
+      this.pantalla = this.resultado;
+    }
+  }
+
+  /*porcentaje():void {
+    this.pantalla+="%";
+    for(var i=0;i<this.pantalla.length;i++){
+      if(this.pantalla[i] === "%") {
+        this.porciento = Number(this.pantalla[i-1])/100;
+        //this.pantalla += this.porciento.toString();
+        break;
+      }
+    }
+  }
+*/
+  resetear():void {
+    this.pantalla = "0";
+  }
+  
 }
